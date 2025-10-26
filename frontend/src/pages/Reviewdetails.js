b6001eaebe1611c401dd74a358e3339f948eee36
NATHAN VARGHESE BIJO <nathan.bijo@gmail.com>
2025-10-27 00:17:01 +0530

    modified reviewdetails.js

--- frontend/src/pages/Reviewdetails.js
+++ frontend/src/pages/Reviewdetails.js
@@ -5,6 +5,7 @@ import { getBook, getReviews, addReview, mongoInsert } from "../api";
 import { getCover } from "../data/books";
 import StarRating from "../components/StarRating";
 import { AuthContext } from "../App";
+useEffect(() => { document.title = "Home - Book Review System"; }, []);
 
 function pastelFromString(s) {
   let h = 0;
